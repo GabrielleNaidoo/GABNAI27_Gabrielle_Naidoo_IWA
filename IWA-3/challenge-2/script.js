d391f6e00d849464d59bcbeabcbcb7b7d47b13d9
Gabrielle Naidoo <naidoogabrielle2@gmail.com>
2023-07-29 21:29:41 +0200

    Addition of index.html and all relevant js files for challenges 1 and 2 of IWA-3

---
+++ IWA-3/challenge-2/script.js
@@ -0,0 +1,5 @@
+import { role as roleNwabisa } from "./nwabisa.js";
+import { role as roleJohannes } from "./johannes.js";
+import { role as roleAlex } from "./alex.js";
+
+console.log(`Roles: ${roleNwabisa}, ${roleJohannes}, ${roleAlex}`);
