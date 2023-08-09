78ed83d4de97f3c5753212861eb43133125ebd5a
Gabrielle Naidoo <naidoogabrielle2@gmail.com>
2023-08-09 23:37:05 +0200

    Addition of files for IWA-6 Challenge 2

--- IWA-6/script.js
+++ IWA-6/script.js
@@ -29,23 +29,26 @@ console.log("Secondary phone is valid numerical string:", secondaryValid);
 /**************************************************************************************************** */
 
 //Challenge 2 :
+const rent = 400;
+const tax = "8%";
+const food = 51.7501;
+const salary = 800;
+const transport = 10.2;
+const hourOfDay = 00;
+const minuteOfDay = 00;
 
-// const rent = 400;
-// const tax = '8%';
-// const food = 51.7501;
-// const salary = 800;
-// const transport = 10.2;
-// const hourOfDay = 00;
-// const minuteOfDay = 00;
+// Only change below this line
 
-// // Only change below this line
-
-// if ((hourOfDay!==null) && (minuteOfDay!== null) )||( (hourOfDay == '00') && (minuteOfDay == '00')) {
-// 	const taxAsDecimal = parseInt(tax) / '100';
-//   const startingAfterTax = salary- taxAsDecimal;
-// 	const balance = startingAfterTax -food - transport- rent;
-//   console.log(balance.toFixed(3));
-// }
-
-// hourOfDay!==undefined
-// minuteOfDay!==undefined
+if (
+  hourOfDay !== null &&
+  minuteOfDay !== null &&
+  hourOfDay !== undefined &&
+  minuteOfDay !== undefined &&
+  hourOfDay == "00" &&
+  minuteOfDay == "00"
+) {
+  const taxAsDecimal = parseInt(tax) / "100";
+  const startingAfterTax = salary - salary * taxAsDecimal;
+  const balance = startingAfterTax - transport - food - rent;
+  console.log(`R${balance.toFixed(2)}`);
+}
