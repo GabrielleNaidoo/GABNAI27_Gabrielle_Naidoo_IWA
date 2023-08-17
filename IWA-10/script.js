876e27ee5150cfca28923d5f89e36f2ad9ede6ed
Gabrielle Naidoo <naidoogabrielle2@gmail.com>
2023-08-17 14:36:31 +0200

    Placement of nested objects

---
+++ IWA-10/script.js
@@ -0,0 +1,131 @@
+const currentYear = new Date().getFullYear();
+
+const holidays = {
+  0: {
+    id: 0,
+    name: "Day of Reconciliation",
+    date: `16 December ${currentYear}`,
+  },
+  1: {
+    id: 1,
+    name: "Workers Day",
+    date: new Date(`1 April ${currentYear}`),
+  },
+  2: {
+    id: 2,
+    name: "Day of Goodwill",
+    date: new Date(`26 December ${currentYear}`),
+  },
+  3: {
+    id: 3,
+    name: "New Year Day",
+    date: new Date(`1 January ${currentYear}`),
+  },
+  4: {
+    id: 4,
+    name: "Womens Day",
+    date: new Date(`9 August ${currentYear}`),
+  },
+  5: {
+    id: 5,
+    name: "Heritage Day",
+    date: new Date(`24 September ${currentYear}`),
+  },
+  6: {
+    id: 6,
+    name: "Christmas Day",
+    date: new Date(`25 December ${currentYear} 13:25`),
+  },
+  7: {
+    id: 7,
+    name: "Youth Day",
+    date: new Date(`16 June ${currentYear}`),
+  },
+  8: {
+    id: 8,
+    name: "Human Rights Day",
+    date: new Date(`21 March ${currentYear}`),
+  },
+};
+
+const christmas = 6;
+const futureId = 9;
+
+// Do not change code above this comment
+console.log(
+  (holidays[futureId] && holidays[futureId]["name"]) ||
+    `ID ${futureId} not created yet`
+);
+
+const copied = Object.assign({}, holidays[christmas]); // Used Object.assign to merge an empty object and holidays object to create copied object so that changes can be made to it without affecting the holidays object.
+copied.name = "X-mas Day";
+const correctDate = new Date(`25 December ${currentYear}`); // Fix of date in copied object stored in variable before being confirmed
+const isEarlier = correctDate.getTime() < holidays[christmas].date.getTime(); // Check before confirming
+console.log("New date is earlier:", isEarlier);
+if (isEarlier) copied.date = correctDate;
+console.log(correctDate);
+/******************************************************************************************/
+
+console.log(
+  `ID change: ${!(holidays[christmas].id === copied.id) && copied.id}`
+);
+console.log(
+  `Name change: ${!(holidays[christmas].name === copied.name) && copied.name}`
+);
+
+// console.log(
+//   `Date change: ${
+//     !(holidays[christmas].date === copied.date) && copied.date.getDate() / copied.date.getMonth() / copied.date.getFullYear()
+//   }`
+// );
+
+console.log(
+  holidays[christmas].date === copied.date
+    ? `Date change: false`
+    : "Date change: " +
+        copied.date.getDate() +
+        "/" +
+        (copied.date.getMonth() + 1) +
+        "/" +
+        copied.date.getFullYear()
+);
+
+/*******************************************************************************************/
+
+console.log(holidays["0"].date.getMonth());
+
+const firstHolidayTimestamp = Math.min(
+  holidays["0"].date,
+  holidays["1"].date,
+  holidays["2"].date,
+  holidays["3"].date,
+  holidays["4"].date,
+  holidays["5"].date,
+  holidays["6"].date,
+  holidays["7"].date,
+  holidays["8"].date
+);
+console.log(firstHolidayTimestamp);
+
+// const lastHolidayTimestamp = Math.max(
+//   holidays[0].date.getTime,
+//   holidays[1].date.getTime,
+//   holidays[2].date.getTime,
+//   holidays[3].date.getTime,
+//   holidays[4].date.getTime,
+//   holidays[5].date.getTime,
+//   holidays[6].date.getTime,
+//   holidays[7].date.getTime,
+//   holidays[8].date.getTime
+// );
+
+// const firstDay = firstHolidayTimestamp.getDate;
+// const firstMonth = firstHolidayTimestamp.getMonth;
+// const lastDay = lastHolidayTimestamp.getDate;
+// const lastMonth = lastHolidayTimestamp.getMonth;
+
+// console.log("{firstDay}/{firstMonth}/{currentYear}");
+// console.log("{lastDay}/{lastMonth}/{currentYear}");
+
+// const randomHoliday = holidays[Math.random];
+// console.log(randomHoliday.date);
--- IWA-9/script.js
+++ IWA-9/script.js
@@ -7,35 +7,33 @@ const size = "large";
 const expenses = {
   food: 51.7501,
   transport: 10.2,
-};
-
-const tax = {
-  734: "3%",
-  234: "20%",
-  913: "12%",
-  415: "38%",
-  502: "42%",
-};
-
-const rent = {
-  none: 0,
-  "small-room": 200,
-  "large-room": 300,
-  "small-apartment": 400,
-  "large-apartment": 800,
-  "small-house": 1200,
-  "large-house": 2400,
+  tax: {
+    734: "3%",
+    234: "20%",
+    913: "12%",
+    415: "38%",
+    502: "42%",
+  },
+  rent: {
+    none: 0,
+    "small-room": 200,
+    "large-room": 300,
+    "small-apartment": 400,
+    "large-apartment": 800,
+    "small-house": 1200,
+    "large-house": 2400,
+  },
 };
 
 // You can change below however you want
 
-const taxAsDecimal = parseInt(tax["913"]) / 100;
+const taxAsDecimal = parseInt(expenses.tax["913"]) / 100;
 const startingAfterTax = salary - salary * taxAsDecimal;
 const type = `${size}-${lodging}`;
 const balance = (
   startingAfterTax -
   expenses.transport -
   expenses.food -
-  rent[type]
+  expenses.rent[type]
 ).toFixed(2);
 console.log(balance);
